51effad490ea001b3396d90b8373c1c56f4358c3
Petr Makeev <essential@inbox.ru>
2022-03-28 20:21:52 +0300

    New folder Example008_IntroMethod

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа поиска максимального числа 
+int a1 = 15;
+int b1 = 20;
+int c1 = 25;
+int a2 = 30;
+int b2 = 35;
+int c2 = 40;
+int a3 = 45;
+int b3 = 40;
+int c3 = 100;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
