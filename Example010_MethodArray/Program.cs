c5c2e5e1cb9812a66c81ab155a4dc7df6813db21
Petr Makeev <essential@inbox.ru>
2022-03-28 21:38:46 +0300

    New folder Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск массива с максимальным числом
+int [] array = {11, 22, 33, 44, 55, 66, 77, 44};
+
+int n = array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index < n)
+{   
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
