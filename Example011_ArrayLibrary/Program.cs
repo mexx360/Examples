f5050c55770f2de4816e35c656f47480b865ae29
Petr Makeev <essential@inbox.ru>
2022-04-08 14:53:09 +0300

    New folder ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+int length = collection.Length;
+int index = 0;
+while (index < length)
+{
+    collection[index] = new Random().Next(1, 10);
+    index++;
+}
+
+}
+
+void PrintArray(int [] collection)
+{
+int count  = collection.Length;
+int position = 0;
+while (position<count)
+{
+Console.WriteLine(collection[position]);
+position++;
+}
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+int count = collection.Length;
+int index = 0;
+int position = 0;
+while (index < count)
+{
+  if(collection[index] == find)
+  {
+    position = index;
+    break;
+  }
+    index++;
+}
+return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+array [4] = 4;
+array [6] = 6;  
+PrintArray(array);
+Console.WriteLine();
+
+int position = IndexOf(array, 4);
+Console.WriteLine(position);
