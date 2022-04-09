3f2f04e134fb193ee8b467ba6e9c24abd915f3fd
Petr Makeev <essential@inbox.ru>
2022-04-09 21:59:42 +0300

    New folder Example013_Method_№4

---
+++ Example013_Method_№4/Program.cs
@@ -0,0 +1,15 @@
+﻿// **Синтаксис цикла "for" на примере 4 Метода - что-то принимают, что-то возвращают**
+
+string Method4(int count, string text)
+{
+    
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(100, "*");
+Console.WriteLine(res);
