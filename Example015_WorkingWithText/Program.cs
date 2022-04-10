8f710eeb5fc277d9c7636934d7362255ba7fa077
Petr Makeev <essential@inbox.ru>
2022-04-10 11:50:17 +0300

    New folder Example015_WorkingWithText

---
+++ Example015_WorkingWithText/Program.cs
@@ -0,0 +1,35 @@
+﻿//===== Работа с текстом.
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы "к" заменить большими "К".
+
+
+string  text = "-Я думаю, - сказал князь, улыбаясь, - что,"
+             +"ежели бы вас послали вместо нашего милого Винценгероде,"
+             +"вы бы взяли приступом согласие прусского короля."
+             +"Вы так красноречивы. Вы дадите мне чаю?";
+             
+             
+// string s = "qwerty"
+//             012345
+// s [3]  // r
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int Length = text.Length;
+    for(int i=0; i < Length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
