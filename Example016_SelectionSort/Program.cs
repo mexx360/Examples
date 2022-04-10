67f6de0c5cb50b902c56ad2335267e938989444d
Petr Makeev <essential@inbox.ru>
2022-04-10 13:24:34 +0300

    New folder Example016_SelectionSort

---
+++ Example016_SelectionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿// Сортировка выбором массива
+
+int[] arr = {1,9,2,8,3,7,4,6,5,5,1,9,2,8,3,7,4,6};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    { 
+    int minPosition = i;
+    
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if(array[j] < array[minPosition]) minPosition = j;
+    }
+    int temporary = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temporary; 
+    }  
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
